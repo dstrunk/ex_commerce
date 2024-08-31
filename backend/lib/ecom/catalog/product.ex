6462f28d7208a3b2e4a80621ba3113c656930f9e
Daniel Strunk <daniel@strunk.me>
2024-08-31 08:48:44 -0400

    Add products

---
+++ backend/lib/ecom/catalog/product.ex
@@ -0,0 +1,21 @@
+defmodule Ecom.Catalog.Product do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "products" do
+    field :name, :string
+    field :description, :string
+    field :price, :integer
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(product, attrs) do
+    product
+    |> cast(attrs, [:name, :description, :price])
+    |> validate_required([:name, :description, :price])
+  end
+end
---
+++ backend/priv/repo/migrations/20240831124235_create_products.exs
@@ -0,0 +1,17 @@
+defmodule Ecom.Repo.Migrations.CreateProducts do
+  use Ecto.Migration
+
+  def change do
+    create table(:products, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string, null: false
+      add :description, :string
+      add :price, :integer, null: false
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index("products", [:name])
+    create constraint("products", "price_must_be_positive", check: "price > 0")
+  end
+end
